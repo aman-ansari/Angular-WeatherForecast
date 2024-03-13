4ee634abf5c88c5e83ed26d9e1a8be2517d986e7
Mohd Aman <sohil@evvoiot.com>
2024-03-13 11:57:20 +0530

    HTML Design

---
+++ src/app/service/current-weather.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CurrentWeatherService {
+
+  constructor() { }
+  API_Key = '78658842978cd50442bdba2cf2bacc0b';
+}
